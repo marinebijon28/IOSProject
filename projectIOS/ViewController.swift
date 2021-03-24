031e1467c920a923b6d126f58a53d5a1e14463e6
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-03-24 09:45:56 +0100

    tab Bar Navigation for display all movies and search movie
    tab Bar Navigation for display all movies and search movie

--- projectIOS/ViewController.swift
+++ projectIOS/ViewController.swift
@@ -22,5 +22,26 @@ class ViewController: UIViewController {
        
     }
     
+//    var url : String = "http://google.com?test=toto&test2=titi"
+//    var request : NSMutableURLRequest = NSMutableURLRequest()
+//    request.URL = NSURL(string: url)
+//    request.HTTPMethod = "GET"
+//
+//    NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+//        var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+//        let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary
+//
+//        if (jsonResult != nil) {
+//            // process jsonResult
+//        } else {
+//           // couldn't load JSON, look at error
+//        }
+    @IBAction func connetedSite(_ sender: UIButton) {
+       
+        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "tabBarHome") as? HomeViewController {
+                     //Display a push navigation
+                     self.navigationController?.pushViewController(vc, animated: true)
+                 }
+    }
 }
 
